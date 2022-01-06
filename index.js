412a8ed1737eb073dfbd7070d9ee146488bbe878
Danny Burke <dburke9607@gmail.com>
2022-01-06 16:19:04 +0000

    adding addtional errors for better error handling

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ function getStsClient(region) {
 }
 
 function isRetryableError(error) {
-  return error instanceof Error && ['IDPCommunicationErrorException', 'InvalidIdentityToken', 'ThrottlingException'].includes(error.code);
+  return error instanceof Error && ['IDPCommunicationErrorException', 'InvalidIdentityToken', 'ThrottlingException', 'ServiceUnavailable', 'ValidationError'].includes(error.code);
 }
 
 async function run() {
